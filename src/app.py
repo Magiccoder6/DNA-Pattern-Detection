e6a1d3e383e3ed6ac5c24aa17399d09fcc634c6a
Shemar Edwards <shemaredwards1990@gmail.com>
2025-03-31 19:26:02 -0500

    Added project structure

---
+++ src/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, render_template, request
+
+from dna import DNA
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+
+@app.post('/test_sequence')
+def test_sequence():
+    data = request.json
+    text_value = data.get('text')
+
+    dna_sequence = text_value
+    dfa = DNA(dna_sequence)
+    result = dfa.analyze()
+    print(result)
+    
+    return jsonify({"result": "result", "found": True})
---
+++ src/dna.py
@@ -0,0 +1,8 @@
+class DNA:
+    def __init__(self, sequence):
+        self.sequence = sequence.upper()  # Ensure uppercase for consistency
+    
+    def find_start_codon():
+        
+        
+
