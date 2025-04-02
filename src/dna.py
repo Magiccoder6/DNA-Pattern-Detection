db468d0cabdf184c896d2a7735ce1c0045ddc7db
Shemar Edwards <shemaredwards1990@gmail.com>
2025-04-01 22:31:35 -0500

    Added transition in one

--- src/dna.py
+++ src/dna.py
@@ -1,64 +1,36 @@
 class DNA:
     def __init__(self, sequence):
-        self.sequence = sequence.upper()  # Ensure uppercase for consistency
-        self.original_sequence = sequence.upper()
+        self.sequence = sequence.upper() + "ε"  # Ensure uppercase for consistency
         self.current_state = "Q0"
-    
-    def _find_start_codon(self):
-        transitions = {
-            'Q0': {'Q1':'A', 'DS0': ['T', 'G', 'C']},
-            'Q1': {'Q2':'T', 'DS0': ['A', 'G', 'C']},
-            'Q2': {'Q3':'G', 'DS0': ['A', 'T', 'C']},
-            'DS0': {'DS0': ['A','T', 'G', 'C']},
-            'Q3': {}
+        self.start_codon_final_state = "Q3"
+        self.cancer_final_state = "Q18"
+        self.huntington_final_state = "Q15"
+
+        self.transitions = {
+            'Q0': {'Q1':'A', 'DS0': ['T', 'G', 'C','ε']},
+            'Q1': {'Q2':'T', 'DS0': ['A', 'G', 'C','ε']},
+            'Q2': {'Q3':['G','ε'], 'DS0': ['A', 'T', 'C','ε']},
+            'DS0': {'DS0': ['A','T', 'G', 'C','ε']},
+            'Q3': {'Q3':['A','T','ε'], 'Q4':'C', 'Q6':'G'},
+            'Q4': {'Q4':'C', 'Q5':'A', 'Q3':['T','ε'], 'Q6':'G'},
+            'Q5': {'Q9':'G','Q3':['T','A','C','ε']},
+            'Q9': {'Q7':'G','Q10':'C','Q3':['T','A','ε']},
+            'Q10': {'Q11':'A','Q3':['T','G','C','ε']},
+            'Q11': {'Q12':'G','Q3':['T','A','C','ε']},
+            'Q12': {'Q13':'C','Q7':'G','Q3':['T','A','ε']},
+            'Q13': {'Q14':'A','Q3':['T','G','C','ε']},
+            'Q14': {'Q15':'G','Q3':['T','A','C','ε']},
+            'Q15': {'Q15':'A','Q15':'T','Q15':'G','Q15':'C','Q15':'ε'},
+            'Q6': {'Q4':'C','Q7':'G','Q3':['T','A','ε']},
+            'Q7': {'Q4':'C','Q7':'G','Q8':'T','Q3':['A','ε']},
+            'Q8': {'Q4':'C','Q16':'G','Q3':['T','A','ε']},
+            'Q16': {'Q4':'C','Q17':'A','Q3':['T','G','ε']},
+            'Q17': {'Q4':'C','Q18':'T','Q3':['A','G','ε']},
+            'Q18': {'Q18':'A','Q18':'T','Q18':'G','Q18':'C','Q18':'ε'}
         }
-        
-        return self._process_sequence(transitions)
     
-    def _find_huntington_disease(self):
-        self.sequence = self.original_sequence
-        transitions = {
-            'Q3' : {'Q4':'C', 'Q3': ['T', 'A']},
-            'Q4' : {'Q5': 'A', 'Q3': ['T','G','C']},
-            'Q5' : {'Q6': 'G', 'Q3': ['A','T','C']},
-            'Q6' : {'Q13': 'C', 'Q3': ['A','T','G']},
-            'Q13' : {'Q14': 'A', 'Q3': ['T','G','C']},
-            'Q14' : {'Q15': 'G', 'Q3': ['A','T','C']},
-            'Q15' : {'Q16': 'C', 'Q3': ['A','T','G']},
-            'Q16' : {'Q17': 'A', 'Q3': ['T','G','C']},
-            'Q17' : {'Q12': 'G', 'Q3': ['A','T','C']},
-            'Q12' : {}
-        }
-        
-        found = self._process_sequence(transitions)
-        if(not found):
-            self.current_state = 'Q3'
-
-        return found
-    
-    def _find_cancer_mutation(self):
-        
-        transitions = {
-            'Q3' : {'Q7':'G', 'Q3': ['A','T']},
-            'Q7' : {'Q8': 'G', 'Q3': ['A','T','C']},
-            'Q8' : {'Q8': 'G', 'Q9':'T', 'Q3': ['A','C']},
-            'Q9' : {'Q10': 'G', 'Q3': ['T','C','A']},
-            'Q10' : {'Q11': 'A', 'Q3': ['T','G','C']},
-            'Q11' : {'Q12': 'T', 'Q3': ['A','G','C']},
-            'Q12' : {}
-        }
-
-        found = self._process_sequence(transitions)
-        
-        if(not found):
-            self.current_state = 'Q3'
-
-        return found
-
     def _process_sequence(self, transitions):
-        index = 0
         for char in self.sequence:
-            index +=1
             dict = transitions[self.current_state]
 
             next_state = None
@@ -70,28 +42,18 @@ class DNA:
             if(next_state):
                 self.current_state = next_state
 
-            print(next_state)
-            if(not next_state):
-                self.sequence = self.sequence[index-1:]
-                break
-        
-        
-        if(transitions[self.current_state]):
-            return False
-        
-        return True
     
     def analyze(self):
-        self.current_state = 'Q0'
+        self._process_sequence(self.transitions)
+        print(self.current_state)
 
-        if(not self._find_start_codon()):
-            return "Start codon (ATG) not found.", False
-        
-        if(self._find_cancer_mutation()):
+        if(self.current_state == self.cancer_final_state):
             return "Possible cancer mutation (GGTGAT) found.", True
-        '''elif(self._find_huntington_disease()):
-            return "Huntington's disease gene (CAG)^3 found", True'''
+        elif(self.current_state == self.huntington_final_state):
+            return "Huntington's disease gene (CAG)^3 found", True
         
+        if(self.current_state != self.start_codon_final_state):
+            return "Start codon (ATG) not found.", False
         
         return "No significant patterns found", True
 
